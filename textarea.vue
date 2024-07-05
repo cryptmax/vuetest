15021361e1d66ff7231eb0b3f39b0ad0fa90a6ee
unimax <95383795+cryptmax@users.noreply.github.com>
2024-07-05 16:25:04 +0300

    Create textarea.vue
    test vue textarea without include vue package

---
+++ textarea.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="form-group">
+    <label :for="name">{{ label }}:</label>
+    <textarea class="form-control" placeholder="Placeholder"
+              :id="name"
+              :name="name"
+              :value="value"
+              @input="update"></textarea>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      name: { required: true },
+      label: { required: false },
+      value: { required: false },
+    },
+    data() {
+      return {}
+    },
+    methods: {
+      update(e) {
+        this.$emit('input', e.target.value);
+      }
+    },
+  }
+</script>
